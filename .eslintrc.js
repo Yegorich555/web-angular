6e97a7c23f6a6f27b5e4c8a4c8481bcb8ae9c3e9
Yegorich555 <yegorich555@gmail.com>
2024-01-08 14:39:01 +0200

    Eslint. Install prettier + configure with ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,50 @@
 module.exports = {
   root: true,
   ignorePatterns: ["projects/**/*"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+  ],
+  plugins: ["json", "prettier", "import", "@typescript-eslint", "unused-imports"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": [
+      "error",
+      {
+        fixToUnknown: true,
+        ignoreRestArgs: false,
+      },
+    ],
+    "prettier/prettier": ["error"],
+    "no-shadow": "off",
+    "no-use-before-define": "off",
+    "require-await": "error",
+    "spaced-comment": ["error", "always"],
+    "unused-imports/no-unused-imports": "error",
+    "no-underscore-dangle": "off",
+    "no-unused-expressions": ["error", { allowShortCircuit: true }],
+    "no-plusplus": "off",
+    "class-methods-use-this": "off",
+    // "max-len": [
+    //   "warn",
+    //   {
+    //     code: 140,
+    //     tabWidth: 2,
+    //     comments: 1000,
+    //     ignoreComments: true,
+    //     ignoreTrailingComments: true,
+    //     ignoreUrls: true,
+    //     ignoreStrings: true,
+    //     ignoreTemplateLiterals: true,
+    //     ignoreRegExpLiterals: true,
+    //   },
+    // ],
+  },
   overrides: [
     {
       files: ["*.ts"],
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-      ],
       rules: {
         "@angular-eslint/directive-selector": [
           "error",
@@ -36,4 +71,15 @@ module.exports = {
       rules: {},
     },
   ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
 };
