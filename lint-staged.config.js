81a39c47c013d7941bf374c4d6214583ee293cf2
Yegorich555 <yegorich555@gmail.com>
2024-01-08 15:55:43 +0200

    Prettier. Disable for html files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,6 @@ module.exports = {
   "*.{css,scss,sass,less}": ["stylelint --fix --cache --allow-empty-input", "prettier --write"],
   "*.{js,ts,json,jsx,tsx}": ["eslint --fix --cache", "prettier --write"],
   // optional: disable prettier for html files (via removing next line)
-  "*.html": ["prettier --write"],
+  // "*.html": ["prettier --write"],
   "*.md": ["markdownlint --fix **/*.md --ignore node_modules"],
 };
